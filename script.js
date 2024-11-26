0f117da304da3e0ff281de166f5dc58593f13dc7
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2024-11-26 13:12:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    this._width = width; // Use private property convention with "_"
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  get width() {
+    return this._width;
+  }
 
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this._width * this._height;
+  }
+}
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side); // Call the parent constructor with equal width and height
+  }
+  getPerimeter() {
+    return 4 * this._width; // Or this._height, as they're the same for a square
+  }
+}
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
+
